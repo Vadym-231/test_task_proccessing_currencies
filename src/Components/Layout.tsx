9f7d5210be3cfd6cbbc10fce37a24d2cff20bfdf
Vadym231 <98084363+Vadym231@users.noreply.github.com>
2023-11-09 03:21:15 +0200

    adding layout

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -3,8 +3,10 @@ import {} from 'antd';
 import Header from "./Header";
 
 export default function ({ component: Component }: { component: FC }) {
-    return <>
-        <Header/>
-        <Component/>
-    </>
+    return <div className={'main-layout'}>
+        <div className={'main-container'}>
+            <Header/>
+            <Component/>
+        </div>
+    </div>
 }
